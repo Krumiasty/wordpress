ef24e155b32910d1cd770e1413ff74177dfafd80
Krumiasty <krruuum@wp.pl>
2014-07-11 11:01:07 +0200

    wordpress

--- wp-content/themes/pulawy/index.php
+++ wp-content/themes/pulawy/index.php
@@ -1,48 +1,6 @@
 <?php
 get_header();
 ?>
-
-
-
-
-<!--
-<?php
-// The Query
-$args= array(
-'post_type' => 'post',
-	);
-$the_query = new WP_Query( $args );
-
-// The Loop
-if ( $the_query->have_posts() ) 
-{
-	echo '<ul>';
-	while ( $the_query->have_posts() )
-	{
-		$the_query->the_post();	
-?>
-		<div id = "mapka">
-		<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
-		src="https://maps.google.pl/maps?ie=UTF8&amp;ll=51.42552,21.974668&amp;spn=0.150921,0.41851&amp;t=m&amp;z=12&amp;output=embed"></iframe><br />
-		<small><a href="https://maps.google.pl/maps?ie=UTF8&amp;ll=51.42552,21.974668&amp;spn=0.150921,0.41851&amp;t=m&amp;z=12&amp;source=embed" style="color:#0000FF;text-align:left">Wyświetl większą mapę</a></small>
-		</div>
-		<div id = "2">
-			<b><?php the_title();?></b>
-		</div>
-		<div id = "3">
-			33333
-		</div>
-		<?php 
-		echo '<li>' . get_the_title() . '</li>';
-	}
-	echo '</ul>';
-} else 
-{
-	echo '// no posts found';
-}
-/* Restore original Post Data */
-wp_reset_postdata();
-?>!-->
            
         <script type="text/javascript">  
         <!--
@@ -59,7 +17,7 @@ wp_reset_postdata();
                 mapa = new google.maps.Map(document.getElementById("mapka"), opcjeMapy);
                  
                 // stworzenie markera
-                var punkt  = new google.maps.LatLng(51.418886, 21.969609);  
+    /*            var punkt  = new google.maps.LatLng(51.418886, 21.969609);  
                 var opcjeMarkera =
                 {
                     position: punkt,
@@ -67,7 +25,7 @@ wp_reset_postdata();
                     draggable: true,
                     clickable: true,
                 }
-                var marker = new google.maps.Marker(opcjeMarkera);
+                var marker = new google.maps.Marker(opcjeMarkera);*/
             } 
         -->
         </script>  
@@ -75,7 +33,67 @@ wp_reset_postdata();
         <!-- tu będzie mapa -->  
         </div> 
         <div>
-        	
+              <div>
+               <?php
+                //  var i = 0;
+                $custom_fields = get_post_custom(2);
+                $my_custom_field = $custom_fields['_my_meta'];
+                foreach ( $my_custom_field as $key => $value ) 
+                {
+                    echo $value[1][0] . "<br />";
+                }
+                $my_meta = get_post_meta($post->ID,’_my_meta’,TRUE);
+                echo $my_meta['number'];
+               // while()
+             //   {
+             //       if($value[i] >)
+            //    }
+              /*  $meta_value = get_post_custom_values( '_my_meta' );
+                foreach ( $meta_value as $key => $value ) 
+                {
+                    echo "$key  => $value ( '_my_meta' )<br />"; 
+                }
+                */
+               ?>
+                </div>
+            <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+            <script type="text/javascript">  
+        	   window.onload = mapaStart();
+               var marker;
+
+        /*        function placeMarker(location)
+                {
+                    if ( marker )
+                    {
+                        marker.setPosition(location);
+                    } 
+                    else 
+                    {
+                        marker = new google.maps.Marker(
+                        {
+                        position: location,
+                        map: mapa
+                        }
+                        );
+                    }
+                }
+
+                google.maps.event.addListener(mapa, 'click', function(event) 
+                {
+                    placeMarker(event.latLng);
+                }
+                );*/
+              
+                var tmp = new Array(51.418886, 21.969609);
+                var myLatlng = new google.maps.LatLng(tmp[0],tmp[1]);
+                alert(tmp);
+                marker = new google.maps.Marker(
+                {
+                    position: myLatlng,
+                    map: mapa,
+                }
+                );
+            </script>
         </div>   
 <?php
 get_footer();
